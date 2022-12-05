f3ac0a49280add4affa37094f8eb66eef6815b7c
Tien Vo V. VN.Danang <tien.vo@monstar-lab.com>
2022-12-05 11:00:38 +0700

    test 39

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello a Cuowng
         </a>
       </header>
     </div>
